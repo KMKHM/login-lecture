917d883b25fe8a90d7a72be2a6d564ff4f70871c
hodumaru <kyun9151@gmail.com>
2022-05-08 17:22:24 +0900

    백엔드 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 //앱 세팅
@@ -10,6 +11,8 @@ app.set("view engine", "ejs");
 const home = require("./src/routes/home")
 app.use("/", home); // 미들웨어 등록
 app.use(express.static(`${__dirname}/src/public`)); 
-
+app.use(bodyParser.json());
+// URL을 통해 전달되는 데이터의 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({extended: true}));
 
 module.exports = app;
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,8 +5,9 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("./home.crtl") // 컨트롤러의 모듈화
 
-router.get("/", ctrl.home); //컨트롤러 분리
+router.get("/", ctrl.output.home); //컨트롤러 분리
 
-router.get("/login", ctrl.login);
+router.get("/login", ctrl.output.login);
+router.post("/login", ctrl.process.login);
 
 module.exports = router; // 모듈 내보내기
